876ebfcd527a743056ba9f18037633b3f5204c21
Arturo <arrome10@gmail.com>
2020-08-09 09:46:01 -0500

    cuarta version

--- ddl_tablas.sql
+++ ddl_tablas.sql
@@ -1,8 +1,10 @@
 -- creacion de tablas del modelo de datos, version 1 (v0.1)
 -- autor : Arturo
 -- fecha : 09/08 
+-- version : v0.5
 
 create table clientes (
 nombre varchar2(30),
 apellidos varchar2(30)
 )
+tablespace tmp;
